681a0df923772a267bcdec64375eafce71d04527
Savindugunasekar <156510802+Savindugunasekar@users.noreply.github.com>
2024-01-24 12:32:04 +0000

    added forms using mustache

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
+
 const express = require('express');
-
 const path = require('path');
+const mustacheExpress = require('mustache-express');
+const JOBS = require('./jobs');
+
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname,'public')))
+// Configure mustache
+app.set('views', path.join(__dirname,'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
 
+// Render the template
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'pages/index.html'));
+    res.render('index', { jobs: JOBS});
+})
+
+app.get('/jobs/:id', (req, res) => {
+    const id = req.params.id;
+    const matchedJob = JOBS.find(job => job.id.toString() === id);
+    console.log('macthedJob',matchedJob)
+    res.render('job', { job: matchedJob});
 })
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server running on https://localhost:${port}`);
-});
+    console.log(`Server running on https://localhost:${port}`)
+})
+
+
